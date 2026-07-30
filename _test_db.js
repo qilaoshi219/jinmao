73c8db3af82b135ba1ba5637ae7e1db215d68743
Ghost_axe <17209484+ghostaxe@user.noreply.gitee.com>
2026-07-30 22:48:04 +0800

    chore: 清理项目内的冗余文件
    移除了临时服务器日志、进程PID文件、敏感凭证文件、测试脚本与样本数据、node_modules锁文件，以及所有非核心的仓库wiki文档文件

--- _test_db.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const { PrismaClient } = require("@prisma/client");
-const p = new PrismaClient();
-p.$connect().then(() => {
-  console.log("Prisma OK");
-  return p.$disconnect();
-}).catch(e => console.error("Prisma FAIL:", e.message));
--- test.js
+++
@@ -1,7 +0,0 @@
-// 测试generate_outline函数是否能正常生成大纲
-const yuanwen = "这是一个测试文章，用于测试大纲生成功能。";
-const pptother = "客户暂未设置额外的ppt风格";
-const generateOutline = require("./node-jinmao/utils/generate_outline.js").generateOutline;
-generateOutline(yuanwen, pptother).then(outline => {
-    console.log("生成的大纲:", outline);
-});
